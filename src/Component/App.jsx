f293e187dbafb91d19ff83eb57a4e39395bcec55
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-19 16:36:28 +0530

    Started creating Wheel component.

---
+++ src/Component/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Wheel from "./Wheel";
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <Wheel/>
+        )
+    }
+}
+
+export default App;
---
+++ src/Component/Wheel.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaForward } from "react-icons/fa6";
+import { FaBackward } from "react-icons/fa6";
+import { IoPlay } from "react-icons/io5";
+import { IoPause } from "react-icons/io5";
+
+class Wheel extends React.Component{
+
+    render(){
+        return(
+            <div className="wheel-container" id="wheel-container">
+            <div className="wheel" id="wheel">
+                <div className="controll" id="menu">
+                    <div>MENU</div>
+                </div>
+                <div className="controll" id="forward">
+                <FaForward />
+                </div>
+                <div className="controll" id="play-pause">
+                    <div>
+                    <IoPlay />
+                    <IoPause />
+                    </div>
+                </div>
+                <div className="controll" id="reverse">
+                <FaBackward />
+                </div>
+            </div>
+
+            <div className="blank" id="blank">
+                
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Wheel;
