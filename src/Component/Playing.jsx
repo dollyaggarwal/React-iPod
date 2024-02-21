ab284dcd229c4b6f792bba9069707f090e279164
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-21 14:24:18 +0530

    completed wheel functionality and event handling on wheel.

--- src/Component/Playing.jsx
+++ src/Component/Playing.jsx
@@ -1,11 +1,56 @@
 import React from "react";
-
+import "../css/playing.css";
 class Playing extends React.Component{
-    render(){
-        return(
-            <>
+    constructor(){
+        super();
+        this.state={
+            currentTime:0,
+        }
+        this.intervalId = "";
+    }
+    //for updating the current music playbreak
+    componentDidMount(){
+        const {audio} = this.props;
+        this.setState({currentTime:audio.currentTime});
+        this.intervalId = setInterval(()=>{
+            this.setState({currentTime:this.props.audio.currentTime});
+        },100);
+    }
 
-            </>
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+    render(){
+        const {songItems,playing,songIndex,audio,songImgUrl} = this.props;
+        var currentTimeRender = Math.floor(this.state.currentTime/60)+":"+Math.floor(this.state.currentTime%60);
+        var durationRender = Math.floor(audio.duration/60)+":"+Math.floor(audio.duration%60);
+
+        const percentageRender = {width:(this.state.currentTime/audio.duration*100)+"%"};
+        if(durationRender === "NaN:NaN"){
+            durationRender:"0:00";
+        }
+        if(Math.floor(this.state.currentTime%60 < 10)){
+         currentTimeRender = Math.floor(this.state.currentTime/60)+":0  "+Math.floor(this.state.currentTime%60);
+        }
+        return(
+           <div className="now-playing-container">
+            <div className="song-details">
+            <img src={songImgUrl} alt="songImg"/>
+            <div>
+                <h6>{songItems}</h6>
+                {playing && <h4 className="play-pause-nav">Playing</h4>}
+                {!playing &&  <h4 className="play-pause-nav">Paused</h4>}
+            </div>
+            </div>
+            <div className="status">
+               {currentTimeRender}
+               <div id="progress">
+               <div style={percentageRender} id="progress-bar"></div>
+
+               </div>
+               {durationRender}
+             </div>
+             </div>
         )
     }
 }
--- src/Component/Wheel.jsx
+++ src/Component/Wheel.jsx
@@ -4,27 +4,34 @@ import { FaForward } from "react-icons/fa6";
 import { FaBackward } from "react-icons/fa6";
 import { IoPlay } from "react-icons/io5";
 import { IoPause } from "react-icons/io5";
+import ZingTouch from "zingtouch";
 
 class Wheel extends React.Component{
-
+    constructor{
+        super();
+        this.this.state={
+            angle:0,
+        }
+    }
     render(){
+        const {changeMenuForward,active,currentMenu,theme,wheelColor} = this.props;
         return(
             <div className="wheel-container" id="wheel-container">
-            <div className="wheel" id="wheel">
+            <div className="wheel" id="wheel" style={{backgroundColor:wheelColor}}>
                 <div className="controll" id="menu">
-                    <div>MENU</div>
+                    <div style={{color:theme}} >MENU</div>
                 </div>
                 <div className="controll" id="forward">
-                <FaForward />
+                <FaForward style={{color:theme}} />
                 </div>
                 <div className="controll" id="play-pause">
                     <div>
-                    <IoPlay />
-                    <IoPause />
+                    <IoPlay style={{color:theme}} />
+                    <IoPause style={{color:theme}} />
                     </div>
                 </div>
                 <div className="controll" id="reverse">
-                <FaBackward />
+                <FaBackward style={{color:theme}} />
                 </div>
             </div>
 
@@ -33,6 +40,61 @@ class Wheel extends React.Component{
             </div>
             </div>
         )
+    }
+
+    wheelControll=(e)=>{
+        const {updateActiveMenu, currentMenu} = this.props;
+        if(e.detail.distanceFromOrigin === 0){
+            this.angle = e.detail.angle;
+        }
+        if(Math.abs(this.angle-e.detail.angle)>100){
+            this.angle = Math.abs(e.detail.angle);
+            if(e.detail.distanceFromLast === 0)
+                return;
+            else if(e.detail.distanceFromLast < 0)
+             updateActiveMenu(1,currentMenu);
+            else
+                updateActiveMenu(0,currentMenu); 
+        }
+        else if(Math.abs(this.angle-e.detail.angle) > 15){
+            this.angle = Math.abs(e.detail.angle);
+            if(e.detail.distanceFromLast === 0)
+            return;
+        else if(e.detail.distanceFromLast > 0)
+        updateActiveMenu(1,currentMenu);
+        else
+        updateActiveMenu(0,currentMenu);
+        }
+    }
+    componentDidMount(){
+        const {changeMenuBackward, togglePlayPause,seekSongForward, seekSongBackward} = this.props;
+
+        const wheelControll = this.wheelControll;
+        const wheel = document.getElementById("wheel");
+        const activeRegion = ZingTouch.Region("wheel");
+        const menuIcon = document.getElementById("menu");
+        const playPause = document.getElementById("play-pause");
+        const reverse = document.getElementById("reverse");
+        const forward = document.getElementById("forward");
+
+        const longTapGesture = new ZingTouch.Tap({
+            maxDelay:10000,
+            numInput:1,
+            tolerance:1,
+        })
+
+        activeRegion.bind(menuIcon,"tap",function(e){
+            changeMenuBackward();
+        })
+        activeRegion.bind(wheel,"rotate",function(e){
+            wheelControll(e);
+        })
+        activeRegion.bind(playPause,"tap",function(e){
+            togglePlayPause();
+        })
+        activeRegion.bind(reverse,"longTapGesture",function(e){
+            seekSongBackward();
+        })
     }
 }
 
