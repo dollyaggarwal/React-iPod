86a30c9525ec39ee9f0210251ad3ddda10f70c23
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-21 12:35:09 +0530

    adding UI and event listeners in different files.

--- src/Component/Song.jsx
+++ src/Component/Song.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 class Song extends React.Component{
     render(){
+        const {songItems, active} = this.props;
         return(
-            <>
-
-            </>
+           <div className="song">
+           <h3>Songs</h3>
+             <ul>
+                    {
+                        songItems.map((element, index)=>{
+                            return active === index? <li key={index} className="active">&nbsp; {element}</li>:
+                            <li key={index}>&nbsp; {element}</li>
+                        })
+                    }
+                </ul>
+           </div>
         )
     }
 }
--- src/Component/WheelColor.jsx
+++ src/Component/WheelColor.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import "../css/theme.css";
 
 class WheelColor extends React.Component{
     render(){
+        const {active} = this.props;
         return(
-            <>
-
-            </>
+            <div className="wheelColor">
+                <h2>Wheel Color Select</h2>
+                <ul>
+                    {["Black","White","Brown"].map((element, index)=>{
+                        return active === index? <li key={index} className="active theme-li">&nbsp; {element}</li>:
+                            <li className="active theme-li" key={index}>&nbsp; {element}</li>   
+                    })}
+                </ul>
+            </div>
         )
     }
 }
